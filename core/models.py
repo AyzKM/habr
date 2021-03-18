925956f588e967dbe69a584b09b36de85c66b4df
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-03-18 17:53:01 +0100

    added authors model and articles&authors pages

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -7,6 +8,38 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     date = models.DateField(default=datetime.now)
+    author = models.ForeignKey(
+        to="Author", 
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name="article",
+        verbose_name="Автор"
+        )
+
+    readers = models.ManyToManyField(
+        to=User,
+        related_name = "readed_articles",
+        blank=True,
+        )
 
     def __str__(self):
         return self.title
+
+class Author(models.Model):
+    user = models.OneToOneField(
+        to=User,
+        related_name="author",
+        verbose_name="пользователь",
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+    )
+
+    nickname = models.CharField(max_length=55)
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+    def __str__(self):
+        return self.nickname
--- core/views.py
+++ core/views.py
@@ -1,16 +1,40 @@
 from django.shortcuts import render, HttpResponse
-from core.models import Article
+from core.models import *
 
 # Create your views here.
 
-def homepage(request):
-    # return HttpResponse("<h2>Hello World</h2>")
-    return render(request, "index.html")
+# def homepage(request):
+#     # return HttpResponse("<h2>Hello World</h2>")
+#     return render(request, "index.html")
 
-def first_article(request):
-    article = Article.objects.first()
+# def first_article(request):
+#     article = Article.objects.first()
+#     return render(
+#         request, 
+#         "article_page.html", 
+#         {"article" : article}
+#         )
+
+def articles(request):
+    articles = Article.objects.all()
     return render(
-        request, 
-        "article_page.html", 
-        {"article" : article}
+        request,
+        "articles.html",
+        {"articles" : articles}
+    )
+
+def authors(request):
+    authors = Author.objects.all()
+    return render(
+        request,
+        "authors.html",
+        {"authors": authors}
+        )
+
+def article(request, id):
+    article = Article.objects.get(id=id)
+    return render(
+        request,
+        "article_page.html",
+        {"article" : article }
         )

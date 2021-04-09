81305ae3c2a86192eb4ea4697debf8d3bc040a99
AyzKM <78482176+AyzKM@users.noreply.github.com>
2021-04-09 13:17:01 +0200

    modified factory and tests

--- core/factories.py
+++ core/factories.py
@@ -5,3 +5,4 @@ class ArticleFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Article
     
+    title = factory.Sequence(lambda n: f"My title number {n}")
--- core/tests.py
+++ core/tests.py
@@ -11,27 +11,21 @@ class HomepageTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '10')
 
-    def test_homepage_with_articles_success(self):
-        # n = 3
-        # for i in range(n):
-        #     article = Article()
-        #     article.title = f'test title{i}'
-        #     article.test = f'test text{i}'
-        #     article.save()
-        
+    def test_homepage_with_articles_success(self):        
         n = 3
         for i in range(n):
             article = ArticleFactory()
-            article.save()
+
+        article.is_active = False
+        article.save()
 
         url = reverse('articles')
         response = self.client.get(url)
         self.assertIn('articles', response.context)
-
+        print(article.title)
         articles = Article.objects.filter(is_active=True)
-        self.assertEqual(articles.count(), n)
-        for i in range (n):
+        self.assertEqual(articles.count(), n - 1)
+
+        for article in articles:
             self.assertContains(response, article.title)
             self.assertContains(response, article.text)
-
-
